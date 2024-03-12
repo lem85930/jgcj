7e743ec6836619024750049e62ec7eafd26528db
wokaotianshi123 <56501836+wokaotianshi123@users.noreply.github.com>
2024-03-12 21:14:04 +0800

    Update xinpq.py

--- xinpq.py
+++ xinpq.py
@@ -9,7 +9,10 @@ all_results = []
 urls = [
     "https://www.yszzq.com/tags/xmlcjjk/",
     "https://www.yszzq.com/tags/xmlcjjk_1",
-    "https://www.yszzq.com/tags/xmlcjjk_2"
+    "https://www.yszzq.com/tags/xmlcjjk_2",
+    "https://www.yszzq.com/tags/xmlcjjk_3",
+    "https://www.yszzq.com/tags/xmlcjjk_4",
+    "https://www.yszzq.com/tags/xmlcjjk_5"
 ]
 
 # 遍历请求多个网址
@@ -18,7 +21,7 @@ for url in urls:
     if response.status_code != 200:
         continue  # 如果请求失败，跳过当前网址
     soup = BeautifulSoup(response.content, "html.parser")
-    elements = soup.find_all(string=re.compile("采集接口"))
+    elements = soup.find_all(string=re.compile("接口|地址|资源库|资源网"))
     for element in elements:
         parent = element.find_parent("a")
         if parent:
@@ -29,7 +32,8 @@ for url in urls:
             else:
                 full_href = f"https://www.yszzq.com{href}"
             # 检查词语是否包含"采集接口"，但不仅仅包含"采集接口"，如果是，则添加到结果中
-            if "采集接口" in title and not title.strip() == "采集接口" and "XML采集接口" not in title:
+            if ("采集接口" in title or "采集地址" in title or "资源库" in title or "json" in title) and not title.strip() == "采集接口" and "XML采集接口" not in title:
+    # 这里是您的处理逻辑
                 all_results.append(f"{title},{full_href}\n")
 
 # 将结果写入pq.txt
